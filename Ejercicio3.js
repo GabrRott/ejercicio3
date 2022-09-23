1c0105befd84293c0e3242f3573b035efe7e2f7a
GabrRott <gabrielroatta@gmail.com>
2022-09-23 11:18:44 -0300

    Initial commit

---
+++ Ejercicio3.js
@@ -0,0 +1,25 @@
+
+let factorialFor= 1;
+
+
+
+for (let i = 1; i <= 10; i++){
+    console.log(factorialFor = factorialFor * i);
+}
+
+let i = 1;
+factorialW = 1;
+while (i <= 10){
+    console.log(factorialW= factorialW * i);
+    i++;
+}
+
+let j = 1;
+factorialW = 1;
+while (j){
+    console.log(factorialW= factorialW * j);
+    if(j == 10){
+        break;
+    }
+    j++;
+}
